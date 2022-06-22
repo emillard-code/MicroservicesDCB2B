846ff229b63b3dbcdae32a7bcf0c7348ad2f7ca0
Emerson Millard <emillard.code@gmail.com>
2022-06-22 12:00:00 -0700

    Added repository class.

---
+++ src/main/java/com/project/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.project.repository;
+
+import com.project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+
+    User findByUserId(Long userId);
+
+}
