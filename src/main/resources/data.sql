b74837f3d01e24c3b0d12ad5499a2dcfa37c9401
Emerson Millard <emillard.code@gmail.com>
2022-06-25 12:00:00 -0700

    Added data.sql file.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values (1, 1, 'a@gmail.com','A-FirstName', 'A-FirstName');
+insert into user values (2, 2, 'b@gmail.com','B-FirstName', 'B-FirstName');
+insert into user values (3, 3, 'c@gmail.com','C-FirstName', 'C-FirstName');
+insert into user values (4, 4, 'd@gmail.com','D-FirstName', 'D-FirstName');
+insert into user values (5, 5, 'e@gmail.com','E-FirstName', 'E-FirstName');
+insert into user values (6, 6, 'f@gmail.com','F-FirstName', 'F-FirstName');
